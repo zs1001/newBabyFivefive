ed4c14a2db188b01e8d1ff3c8ae31b81f9c45f95
Administrator <Administrator@WINDOWS-RHOESR3>
2018-10-09 10:04:57 +0800

    new

---
+++ src/main/java/com/dooshen/newbabyfivefive/App.java
@@ -0,0 +1,13 @@
+package com.dooshen.newbabyfivefive;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
