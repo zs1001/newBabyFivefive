acbed77803af588734a7dacffeab6c090b124de1
Administrator <Administrator@WINDOWS-RHOESR3>
2018-10-11 11:13:23 +0800

    main 调用kcwAPI

--- src/main/java/com/dooshen/newbabyfivefive/App.java
+++ src/main/java/com/dooshen/newbabyfivefive/App.java
@@ -1,5 +1,11 @@
 package com.dooshen.newbabyfivefive;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
+import com.dooshen.newbabyfivefive.common.HttpClientUtils;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,14 @@ public class App
 {
     public static void main( String[] args )
     {
+    	//http://f.apiplus.net/cqssc-20.xml
+    	
+    	String url= "http://f.apiplus.net/cqssc-20.json";
+    	
+    	String kj = HttpClientUtils.sendHttpGet(url);
+    	System.out.println(kj);
         System.out.println( "Hello World!" );
+        
+      
     }
 }
