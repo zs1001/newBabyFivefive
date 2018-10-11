6ff8dba1cc54434707ea479a371af3c993f51189
Administrator <Administrator@WINDOWS-RHOESR3>
2018-10-11 11:13:23 +0800

    main 调用kcwAPI

---
+++ src/main/java/com/dooshen/newbabyfivefive/common/FastJsonUtils.java
@@ -0,0 +1,21 @@
+package com.dooshen.newbabyfivefive.common;
+
+
+public final class FastJsonUtils {
+	
+//	使用fastjson将不规则的json串转成java对象 
+//	JSONObject obj = JSON.parseObject(requestResult); 
+//	JSONObject 是一个map嵌套map的对象
+//
+//	public static final Object parse(String text); // 把JSON文本parse为JSONObject或者JSONArray 
+//	public static final JSONObject parseObject(String text); // 把JSON文本parse成JSONObject 
+//	public static final T parseObject(String text, Class clazz); // 把JSON文本parse为JavaBean 
+//	public static final JSONArray parseArray(String text); // 把JSON文本parse成JSONArray 
+//	public static final List parseArray(String text, Class clazz); //把JSON文本parse成JavaBean集合 
+//	public static final String toJSONString(Object object); // 将JavaBean序列化为JSON文本 
+//	public static final String toJSONString(Object object, boolean prettyFormat); // 将JavaBean序列化为带格式的JSON文本 
+//	public static final Object toJSON(Object javaObject); 将JavaBean转换为JSONObject或者JSONArray。 
+	
+	
+	
+}
